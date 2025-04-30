da0b5da33446883ca4993bb6b82948fb21c1c803
sadhin28 <taosifsadhin113@gmail.com>
2025-04-30 19:32:03 +0600

    add watch and price

--- src/components/watch/Watch.jsx
+++ src/components/watch/Watch.jsx
@@ -2,11 +2,10 @@ import './Watch.css'
 
 const Watch = ({watch}) => {
     
-    const {name,price,img}=watch
-   console.log(typeof(img))
+    const {name,price}=watch
+
     return (
         <div>
-            <img src={img} alt="" />
             <h4>Watch : {name} </h4>
             <p>Price: {price}</p>
         </div>
