3a346158bdd93f5c2d8d75fdcb79bc28f424dc85
sadhin28 <taosifsadhin113@gmail.com>
2025-04-30 18:39:12 +0600

    add sunglass

--- src/components/sunglass/Sunglass.jsx
+++ src/components/sunglass/Sunglass.jsx
@@ -1,8 +1,16 @@
 import './Sunglass.css'
+import Watch from '../watch/Watch';
+import { add, multiply } from '../../Utils/Calculate';
+// import add from '../../Utils/Calculate';
 const Sunglass = () => {
+   const first = 55;
+   const second = 199;
+   const sum = add(first,second)
+   const mul= multiply(first,second) 
+  
     return (
         <div>
-            
+            <Watch></Watch>
         </div>
     );
 };
