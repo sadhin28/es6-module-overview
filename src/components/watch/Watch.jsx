2d23ea24547fcbf7d19aba191e416e84299fc5f2
sadhin28 <taosifsadhin113@gmail.com>
2025-04-30 19:28:05 +0600

    add json

--- src/components/watch/Watch.jsx
+++ src/components/watch/Watch.jsx
@@ -1,9 +1,14 @@
 import './Watch.css'
 
-const Watch = () => {
+const Watch = ({watch}) => {
+    
+    const {name,price,img}=watch
+   console.log(typeof(img))
     return (
         <div>
-            
+            <img src={img} alt="" />
+            <h4>Watch : {name} </h4>
+            <p>Price: {price}</p>
         </div>
     );
 };
